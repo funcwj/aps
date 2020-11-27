5b7142d57c232fb370190c1974b7cd151db553a8
funcwj <funcwj@foxmail.com>
2020-11-27 16:49:15 +0800

    [docker] add test script for rnnt package

--- .ci/install_test_deps.sh
+++ .ci/install_test_deps.sh
@@ -2,18 +2,19 @@
 
 set -eu
 
+nj=$(nproc)
+work_dir=$PWD
 # Pyhon dependencies
 sudo apt-get update
 sudo apt-get install -y libsndfile-dev ffmpeg
 pip install --upgrade pip wheel
 cat < requirements.txt | grep -v -E "warp_rnnt|horovod|kenlm" > requirements_cpu.txt && pip install -r requirements_cpu.txt
 pip install numba==0.48
-work_dir=$PWD
 # RNNT (can run on CPU)
 git clone https://github.com/HawkAaron/warp-transducer.git && cd warp-transducer
-mkdir build && cd build && cmake .. && make -j $(nproc)
+mkdir build && cd build && cmake .. && make -j $nj && sudo make install && sudo ldconfig -v
 cd ../pytorch_binding && python setup.py install && cd $work_dir && rm -rf warp-transducer
 # sentencepiece
 git clone https://github.com/google/sentencepiece.git && cd sentencepiece
-mkdir build && cd build && cmake .. && make -j $(nproc)
+mkdir build && cd build && cmake .. && make -j $nj
 sudo make install && sudo ldconfig -v && cd $work_dir && rm -rf sentencepiece
--- docker/README.md
+++ docker/README.md
@@ -10,13 +10,13 @@ cat Dockerfile | docker build --build-arg CUDA_VERSION=10.0 \
 # pytorch 1.6.0 with cuda 10.2, python 3.8
 cat Dockerfile | docker build --build-arg CUDA_VERSION=10.2 \
     --build-arg CUDNN=7 \
-    --build-arg PYTHON_VERSION=3.8 \
+    --build-arg PYTHON_VERSION=3.8.6 \
     --build-arg PYTORCH_VERSION=1.6.0 \
     --tag aps-pt1.6.0-py3.8.0-cuda10.2-ubuntu18.04:v1.0 -
 # pytorch 1.7.0 with cuda 11.0, python 3.8
 cat Dockerfile | docker build --build-arg CUDA_VERSION=11.0 \
-    --build-arg CUDNN=7 \
-    --build-arg PYTHON_VERSION=3.8 \
+    --build-arg CUDNN=8 \
+    --build-arg PYTHON_VERSION=3.8.6 \
     --build-arg PYTORCH_VERSION=1.7.0 \
-    --tag aps-pt1.7.0-py3.8.0-cuda11.0-ubuntu18.04:v1.0 -
+    --tag aps-pt1.7.0-py3.8.6-cuda11.0-ubuntu18.04:v1.0 -
 ```
