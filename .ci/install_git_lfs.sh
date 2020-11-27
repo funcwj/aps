4ff9bd4703dec50d0a087960dd7a4d5f6befc0dd
funcwj <funcwj@foxmail.com>
2020-11-27 17:56:22 +0800

    [.ci] try to fix git-lfs envs (2nd)

--- .ci/install_git_lfs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-sudo apt-get install -y curl
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
