1b8f209bb244810f56b3fb5e63fa02315b615027
funcwj <funcwj@foxmail.com>
2020-11-27 12:37:33 +0800

    use git-lfs to track checkpoint

--- .ci/install_test_deps.sh
+++ .ci/install_test_deps.sh
@@ -8,11 +8,12 @@ sudo apt-get install -y libsndfile-dev ffmpeg
 pip install --upgrade pip wheel
 cat < requirements.txt | grep -v -E "warp_rnnt|horovod|kenlm" > requirements_cpu.txt && pip install -r requirements_cpu.txt
 pip install numba==0.48
+work_dir=$PWD
 # RNNT (can run on CPU)
 git clone https://github.com/HawkAaron/warp-transducer.git && cd warp-transducer
 mkdir build && cd build && cmake .. && make -j $(nproc)
-cd ../pytorch_binding && python setup.py install && cd ../../ && rm -rf warp-transducer
+cd ../pytorch_binding && python setup.py install && cd $work_dir && rm -rf warp-transducer
 # sentencepiece
 RUN git clone https://github.com/google/sentencepiece.git && cd sentencepiece
 mkdir build && cd build && cmake .. && make -j $(nproc)
-make install && ldconfig -v && cd ../../ && rm -rf sentencepiece
+make install && ldconfig -v && cd $work_dir && rm -rf sentencepiece
