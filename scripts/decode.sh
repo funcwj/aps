f7ae5bdf659b75d4a41d9fea371509f74bb9bbea
funcwj <funcwj@foxmail.com>
2021-02-09 05:56:45 +0800

    [aps] add reduce options for compute_wer.py

--- scripts/decode.sh
+++ scripts/decode.sh
@@ -53,7 +53,7 @@ exp_dir=exp/$mdl_id/$exp_id
 mkdir -p $dec_dir $dec_dir/log
 
 dec_prefix=beam${beam_size}_eos${eos_threshold}_lp${len_penalty}
-[ $len_norm ] && dec_prefix=${dec_prefix}_norm
+$len_norm && dec_prefix=${dec_prefix}_norm
 
 if [ -z $log_suffix ]; then
   log_suffix=$dec_prefix
