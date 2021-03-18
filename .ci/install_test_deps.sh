231051bd1bb6e7e2ae3c17bbe1ebd976596dc957
funcwj <funcwj@foxmail.com>
2021-03-18 15:21:36 +0800

    [.ci] fix enviroment setup for github action

--- .ci/install_test_deps.sh
+++ .ci/install_test_deps.sh
@@ -8,9 +8,11 @@ sudo apt-get update
 sudo apt-get install -y libsndfile-dev ffmpeg curl
 # python dependencies
 pip install --upgrade pip wheel
-cat < requirements.txt | grep -v -E "warp_rnnt|horovod|kenlm" > requirements_cpu.txt && pip install -r requirements_cpu.txt
+pip install -r requirements.txt
 pip install numba==0.48
 # RNNT (can run on CPU)
 git clone https://github.com/HawkAaron/warp-transducer.git && cd warp-transducer
 mkdir build && cd build && cmake .. && make -j $nj && sudo make install && sudo ldconfig -v
 cd ../pytorch_binding && python setup.py install && cd $work_dir && rm -rf warp-transducer
+# kenlm
+pip install https://github.com/kpu/kenlm/archive/master.zip
