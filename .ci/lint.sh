8f31266dd6ac4fc37b4a01881a36f194d877bcd3
funcwj <funcwj@foxmail.com>
2021-08-20 17:38:43 +0800

    fix lint dependency

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -3,11 +3,13 @@
 set -eu
 
 sudo apt-get install -y shellcheck
+cat requirements.txt | grep -E "flake8|cpplint" > requirements_lint.txt
+pip install -r requirements_lint.txt
 
 # for shell
 shellcheck -e SC2086,SC2002 -x tests/python/*.sh utils/subword.sh scripts/*.sh
 shellcheck -e SC2086,SC2001,SC2002,SC2010 -x examples/*/*/run.sh
 # for python
-pip install flake8 && flake8 --version && flake8
+flake8
 # for c++
 cpplint --recursive csrc
